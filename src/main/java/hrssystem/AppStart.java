d17f5bd21561a5f540fae8216f366568d23f7cef
kim ri won <lelabo7317@gmail.com>
2025-02-25 11:52:19 +0900

    25-02-25 1152i commit

---
+++ src/main/java/hrssystem/AppStart.java
@@ -0,0 +1,11 @@
+package hrssystem;
+
+import org.springframework.boot.SpringApplication;
+
+SpringApplication
+public class AppStart {
+    public static void main(String[] args) {
+        // Start the Spring Boot application
+        SpringApplication.run(AppStart.class, args);
+    }
+}
